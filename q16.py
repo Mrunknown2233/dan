79a870dbf8c456ddab3b29d291d67d8feb595466
Mrunknown <dedhiasoumya299@gmail.com>
2025-10-31 10:13:38 +0530

    TP

--- q16.py
+++ q16.py
@@ -3,8 +3,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 # Create data
-x = np.linspace(-10, 10, 100) # 100 points from -10 to 10
-y = x ** 2 # y = x squared
+# x = np.linspace(-10, 10, 100) # 100 points from -10 to 10
+x =[1,2,3,4,9,5,6,7]
+y = [i ** 2 for i in x]
+# y = x ** 2 # y = x squared
 # Plot
 plt.plot(x, y)
 plt.title("Graph of y = x²")
@@ -16,3 +18,13 @@ plt.savefig("y_equals_x2.png", bbox_inches='tight')
 plt.savefig("y_equals_x2.pdf", bbox_inches='tight')
 plt.savefig("y_equals_x2.svg", bbox_inches='tight')
 plt.show()
+
+# What is the explosion effect of pie chart???
+# 1. The explosion effect in a pie chart is used to highlight or emphasize a particular slice
+# 2. It visually seperated one or more slives from the rest of the chart.
+# 3. This seperation draws the viewr's attention to specific data.
+# 4. It helps in comparing important segemnets clearly.
+# 5. The exploded slice usually represents a key value or important information
+# 6. The effect improves the visual appeal and readability of the chart.
+# 7. It can be used to show focus on highest or lowest percentage data.
+# 8. In short, the explosion effect makes selected slices stand out for emphasis or clarity
